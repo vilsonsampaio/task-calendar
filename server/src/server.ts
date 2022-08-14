7626476d351c291611208362bffe23c34de2d070
Vilson Sampaio <vilson.sampaio04@gmail.com>
2022-08-14 20:01:15 -0300

    chore(api): setting cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,7 +5,11 @@ import { router } from './routes';
 
 const app = express();
 
-app.use(cors());
+app.use(cors({ 
+  origin: process.env.NODE_ENV === 'development' 
+    ? 'http://localhost:3000' 
+    : process.env.APP_FRONT_URL  
+}));
 app.use(express.json());
 app.use(router);
 
