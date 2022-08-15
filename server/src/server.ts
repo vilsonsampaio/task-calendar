9fac0633be2e4fc0d17f6eb3c3865fbd5145ccad
Vilson Sampaio <vilson.sampaio04@gmail.com>
2022-08-14 21:17:54 -0300

    chore(api): add error handler

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,15 @@
 import express from 'express';
+import 'express-async-errors';
 import cors from 'cors';
 
 import { router } from './routes';
+import errorHandler from './errors/handler';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(router);
+app.use(errorHandler);
 
 app.listen(3000, () => console.log("Server is running"));
