8511da4727b55a52b3a48a18d2bbab6395777903
Vilson Sampaio <vilson.sampaio04@gmail.com>
2022-08-14 21:03:31 -0300

    fix(api): removing cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,11 +5,7 @@ import { router } from './routes';
 
 const app = express();
 
-app.use(cors({ 
-  origin: process.env.NODE_ENV === 'development' 
-    ? 'http://localhost:3000' 
-    : process.env.APP_FRONT_URL  
-}));
+app.use(cors());
 app.use(express.json());
 app.use(router);
 
